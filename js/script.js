80a2dd43fd7d96cf714c48417c2a1629206870cb
Bruno Dorigon <bdorigon@hotmail.com>
2022-10-03 09:56:02 -0300

    galeria imagem

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ativar links menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link){
@@ -10,3 +11,58 @@ function ativarLink(link){
 }
 
 links.forEach(ativarLink)
+
+
+// Ativar items do orçamento
+const url = new URLSearchParams(location.search);
+
+function ativarProduto(url){
+  const elemento = document.getElementById(url);
+  if(elemento){
+    elemento.checked = true
+  }
+}
+
+url.forEach(ativarProduto);
+
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  console.log(ativa)
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas);
+
+
+// Galeria de bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagem img');
+const galeriaContainer = document.querySelector('.bicicleta-imagem')
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 1000px)').matches;
+  if(media) {
+    galeriaContainer.prepend(img);
+  }
+  console.log(img)
+}
+
+function eventosGaleria(img){
+  img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria);
